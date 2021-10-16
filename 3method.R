2a6ee76da305bb6ad7763c8af68019c4158d3c75
sliu <91963940+sliu736@users.noreply.github.com>
2021-10-16 05:14:59 +0000

    model们的r语言版本
    lasso ridge 结果和Python不同，且r方和普通线性回归衡量标准不同；
    加入年龄分段的model；
    模型横向比较困难，考虑引入新数据集进行检测？

--- 3method.R
+++ 3method.R
@@ -19,14 +19,73 @@ best_lamb <- cv.out$lambda.min
 predict(fit_ridge, type='coefficients', s=best_lamb)
 
 r_square<-fit_ridge$dev.ratio[which(fit_ridge$lambda==best_lamb)]
-#======lasso===========
-
+#=======lasso===========
 fit_lasso <- glmnet(x, y, family="gaussian", alpha=1)
 plot(fit_lasso, xvar='lambda', label = TRUE)
 
 cv.out <- cv.glmnet(x, y, alpha=1)
 plot(cv.out)
 best_lamb<-cv.out$lambda.min
-lasso.coef <- predict(fit_lasso, type='coefficients', s=best_lamb)
+predict(fit_lasso, type='coefficients', s=best_lamb)
+fit_lasso$dev.ratio[which(fit_lasso$lambda == best_lamb)]
 
-r_square<-fit_lasso$dev.ratio[which(fit_lasso$lambda == best_lamb)]
+#=======random forest=========
+library(randomForest)
+fit_rf<-randomForest(BODYFAT~., data = dat_del)
+summary(fit_rf)
+importance(fit_rf)
+
+#=========split in age========
+dat_del$age_group<-'g2'
+dat_del$age_group[which(dat$AGE<=40)]<-'g1'
+
+
+dat_del$age_group<-'g2'
+dat_del$age_group[which(dat$AGE<=40)]<-'g1'
+dat_del$age_group[which(dat$AGE>60)]<-'g3'
+
+
+
+boxplot(BODYFAT~age_group,data = dat_del)
+
+plot(BODYFAT~factor(age_group)*factor(ADIPOSITY),data=dat_del)
+
+#======age group under 40======
+sub<-which(dat_del$age_group=='g1')
+fit<-lm(BODYFAT~.,dat_del[sub,-16])
+step(fit)
+#Stepwise model
+fit<-lm(BODYFAT ~ ABDOMEN + HIP + THIGH + WRIST,dat=dat_del[sub,])
+summary(fit)
+
+#=====age group under 60=====
+sub<-which(dat_del$age_group=='g2')
+fit<-lm(BODYFAT~.,dat_del[sub,-16])
+step(fit)
+#Stepwise model
+fit<-lm(BODYFAT ~  AGE + NECK + ABDOMEN + ANKLE + FOREARM + WRIST,dat=dat_del[sub,])
+summary(fit)
+#drop not significant
+fit<-lm(BODYFAT ~  NECK + ABDOMEN + ANKLE + FOREARM + WRIST,dat=dat_del[sub,])
+
+summary(fit)
+
+#=====age group above 60=====
+sub<-which(dat_del$age_group=='g3')
+fit<-lm(BODYFAT~.,dat_del[sub,-16])
+step(fit)
+#Stepwise model
+fit<-lm(BODYFAT ~ ABDOMEN + ANKLE + BICEPS + FOREARM,dat=dat_del[sub,])
+summary(fit)
+#drop not significant
+fit<-lm(BODYFAT ~ ABDOMEN + ANKLE ,dat=dat_del[sub,])
+summary(fit)
+
+
+
+#=========poly==========
+#ABDOMEN/HIP
+#ADIPOSITY^2
+
+
+
